7d9958f95fffdfe85b3d1694999079a89d9e118a
aries733 <ariana.atanazio@academico.ufpb.br>
2022-09-30 10:46:13 -0300

    modulo de dados e index.js

---
+++ js/moduloDados.js
@@ -0,0 +1,38 @@
+/**Dentro deste array existem usuarios com esta estrutura:
+ * - id
+ * - nomeUsuario
+ * - senha
+ * - email
+ */
+
+const usuarios = [
+  {
+    id: 1,
+    nomeUsuario: "ariana",
+    senha: "12345",
+    email: "ariana@email.com",
+  },
+  {
+    id: 2,
+    nomeUsuario: "jose-almir",
+    senha: "12345",
+    email: "jose@email.com",
+  },
+  {
+    id: 3,
+    nomeUsuario: "amanda",
+    senha: "12345",
+    email: "amanda@email.com",
+  },
+  {
+    id: 4,
+    nomeUsuario: "jessica",
+    senha: "12345",
+    email: "jessica@email.com",
+  },
+];
+
+export default usuarios;
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
