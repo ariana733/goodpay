c1d4b1b1e878b47127b1558f12f79509398c7bb2
Daniflav94 <danyelly_mell@hotmail.com>
2022-10-04 13:32:27 -0300

    Adição de um teste de cálculo para verificação de robôs

--- js/index.js
+++ js/index.js
@@ -7,23 +7,56 @@ import usuarios from "./moduloDados.js"
 const btnEntrar = document.getElementById("btnEntrar")
 const alertaLogin = new bootstrap.Modal("#alertaLogin")
 
+const alertaCaptcha = new bootstrap.Modal("#erroCaptcha")
+const spanCaptcha = document.getElementById("captcha")
+
+const captchaGerado = geradorNumeros()
+
+spanCaptcha.innerText = `Resolva a Operação: ${captchaGerado.num1ENum2}`
+
 btnEntrar.addEventListener("click", () => {
     const inputLogin = document.getElementById("login").value
     const inputSenha = document.getElementById("senha").value
 
+    const inputCaptcha = parseInt(document.getElementById("input-captcha").value)
+
     let usuarioValido;
 
-    for(let usuario of usuarios) {
-        if(usuario.nomeUsuario == inputLogin) {
-            usuarioValido = usuario;
-            break
+    if(captchaGerado.resultado == inputCaptcha){
+        for(let usuario of usuarios){
+            if(usuario.nomeUsuario == inputLogin){
+                usuarioValido = usuario;
+                if(inputSenha == usuarioValido.senha){
+                    window.location.pathname = '/app.html' 
+                }else{
+                    /*modal de erro de senha precisa ser implementado
+                    alertaLoginSenha.show()
+                    break*/
+                }
+            }else{
+                /*modal de erro de usuario precisa ser implementado
+                alertaLoginUsuario.show()
+                break**/
+            }
         }
+    }else{
+        alertaCaptcha.show()
+
+    }
+})
+    
+//Função geradora de números para verificação de captcha
+function geradorNumeros(){
+
+    let num1 = Math.floor(Math.random() * 100)
+    let num2 = Math.floor(Math.random() * 50)
+
+    let captcha  = {
+    num1: num1,
+    num2: num2,
+    num1ENum2: num1 + ' + ' + num2 + ' = ?',
+    resultado: num1 + num2
     }
 
-    if(inputSenha == usuarioValido.senha) {
-        // redirectiona o usuário para essa página html
-        window.location.pathname = '/app.html'
-    } else {
-        alertaLogin.show()
-    }
-});
+    return captcha
+}
